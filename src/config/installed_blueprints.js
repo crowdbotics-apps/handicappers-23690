d99e90e6e95bc7cd60363e0181b1d9254e37d3fc
Crowdbotics <team@crowdbotics.com>
2021-01-06 04:35:13 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn21190470', human_name: 'Sign In 2 1', access_route: 'SignIn21190470'},
 { name: 'UserProfile3190468', human_name: 'User Profile 3', access_route: 'UserProfile3190468'},
 { name: 'BlankScreen0190465', human_name: 'Blank Screen 0', access_route: 'BlankScreen0190465'},
 
